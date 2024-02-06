9d66077981525f181e85ab9a92894cd2fd1256b4
RaghavendraBtech <raghavendraeee3@gmail.com>
2024-02-06 14:03:17 +0530

    RAndDServer-Eureka

---
+++ MicroserviceProj01-RAndDServer-Eureka/src/main/java/com/raghu/MicroserviceProj01RAndDServerEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.raghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceProj01RAndDServerEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProj01RAndDServerEurekaApplication.class, args);
+	}
+
+}
