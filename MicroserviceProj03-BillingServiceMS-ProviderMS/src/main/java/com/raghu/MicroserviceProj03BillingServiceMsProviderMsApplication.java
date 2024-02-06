a6524e76a0b237e3bb7b448901763a0a35204be1
RaghavendraBtech <raghavendraeee3@gmail.com>
2024-02-06 14:12:14 +0530

    MSproj03-ProviderMS

---
+++ MicroserviceProj03-BillingServiceMS-ProviderMS/src/main/java/com/raghu/MicroserviceProj03BillingServiceMsProviderMsApplication.java
@@ -0,0 +1,15 @@
+package com.raghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MicroserviceProj03BillingServiceMsProviderMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProj03BillingServiceMsProviderMsApplication.class, args);
+	}
+
+}
