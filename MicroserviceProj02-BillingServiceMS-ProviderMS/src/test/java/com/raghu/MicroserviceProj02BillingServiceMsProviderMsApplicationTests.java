dd1416de689590595ce996674f7b6184bc3db9e6
RaghavendraBtech <raghavendraeee3@gmail.com>
2024-02-07 16:48:43 +0530

    MSproj02-ProviderMS

---
+++ MicroserviceProj02-BillingServiceMS-ProviderMS/src/test/java/com/raghu/MicroserviceProj02BillingServiceMsProviderMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProj02BillingServiceMsProviderMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
